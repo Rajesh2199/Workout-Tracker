012555c6a503f0aa17470dde0fe8a4fed010a7bc
baduraje@isu.edu <baduraje@isu.edu>
2020-09-23 02:44:10 -0600

    added function for workouts/range

--- Develop/routes/api.js
+++ Develop/routes/api.js
@@ -34,6 +34,16 @@ router.get("/api/workouts", (req, res)=>{
 });
 
 
+router.get("/api/workouts/range", (req, res)=>{
+    Workout.find({})
+        .then(dbWorkout =>{
+            res.json(dbWorkout);
+        })
+        .catch(err =>{
+            res.status(400).json(err);
+        });
+});
+
 
 
 
