035d5aeddfc3f7aff18ee0df26a0a9c8a2daf36f
baduraje@isu.edu <baduraje@isu.edu>
2020-09-23 02:08:12 -0600

    added router.put function , made changes to router.post function

--- Develop/routes/api.js
+++ Develop/routes/api.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const Workout = require("../models/workout.js");
 
 router.post("/api/workouts", (req, res)=>{
-    Workout.create()
+    Workout.create({})
         .then(dbWorkout =>{
             res.json(dbWorkout);
         })
@@ -10,6 +10,30 @@ router.post("/api/workouts", (req, res)=>{
             res.status(400).json(err);
         });
 });
+
+router.put("/api/workouts/:id", (req, res)=>{
+    Workout.create()
+    .then(dbWorkout=>{
+        res.json(dbWorkout);
+    })
+    .catch(err =>{
+        res.status(400).json(err);
+    });
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 module.exports = router;
